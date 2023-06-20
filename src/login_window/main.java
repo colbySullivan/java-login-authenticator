28d232a6161bc4c42342f250efb53b27b44693a2
Colby Sullivan <88957296+colbySullivan@users.noreply.github.com>
2023-06-20 13:35:19 -0500

    Eclipse files

--- src/login_window/main.java
+++ src/login_window/main.java
@@ -3,9 +3,13 @@ package login_window;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
+import java.util.HashMap;
 
 import javax.swing.*;
 
@@ -57,13 +61,15 @@ public abstract class main implements ActionListener{
 		register.setBackground(Color.BLUE);
 		panel.add(register);
 		
+		//HashMap<String, String> credentials = new HashMap<String, String>();
+		String Username = username.getText();
+    	String Password1 = Password.getText();
+    	HashMap credentials = check_credentials(Username, Password1);
+		
 		// Login button action
 		login.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-            	String Username = username.getText();
-            	String Password1 = Password.getText();
-            	boolean login = check_credentials(Username, Password1);
-            	if(login)
+            	if(credentials.containsKey(Username) && credentials.get(Username).equals(Password1))
             		JOptionPane.showMessageDialog(null, "Login correct");
             	else
             		JOptionPane.showMessageDialog(null, "Login incorrect");
@@ -75,33 +81,44 @@ public abstract class main implements ActionListener{
             public void actionPerformed(ActionEvent e) {
             	String Username = username.getText();
             	String Password1 = Password.getText();
-            	create_account(Username, Password1);
+            	try {
+					create_account(Username, Password1);
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
             	JOptionPane.showMessageDialog(null, "Account created");
             }
         });
 		frame.setVisible(true);
 	 }
 
-	  static boolean check_credentials(String username, String password) {
+	  static HashMap check_credentials(String username, String password) {
+		HashMap<String, String> credentials = new HashMap<String, String>();
 		File loginf = new File("users.txt");
-		boolean login = false;
   		try (Scanner read = new Scanner(loginf)) {
   			read.useDelimiter("\\n|,");
   			while(read.hasNext()){
   			   String user = read.next();
   			   String pass = read.next();
-  			   if(user.equals(username) && pass.equals(password))
-  				   login = true;
+  			   credentials.put(user, pass);
   			}
   		} catch (FileNotFoundException e1) {
 				// TODO Auto-generated catch block
   			System.out.print("File not found");
 				//e1.printStackTrace();
 			}
-		return login;
+		return credentials;
 	}
-	  static void create_account(String username, String password1) {
-		  
+	  static void create_account(String username, String password1) throws IOException {
+		File fileName = new File("users.txt");
+		FileWriter fw = new FileWriter(fileName, true);
+	    BufferedWriter bw = new BufferedWriter(fw);
+	    String formatted = username + "," + password1;
+	    bw.newLine();
+	    bw.write(formatted);
+	    bw.newLine();
+	    bw.close();
 	  }
 	
 }
